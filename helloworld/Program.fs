458b47e35c760b1d1df571328d9fb14b73add29c
Josh <zylo.codes@gmail.com>
2022-10-06 14:36:11 -0500

    for in loop

--- helloworld/Program.fs
+++ helloworld/Program.fs
@@ -2,8 +2,7 @@
 
 [<EntryPoint>]
 let main argv =
-  for i in 0..argv.Length-1 do 
-    let person = argv[i]
+  for person in argv do 
     printfn "Hello %s from my F# program!" person
   printfn "Nice to meet you."
   0
