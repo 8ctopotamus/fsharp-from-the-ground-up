ab3b2b339524bd2d4dc38579a41e15a4f3623982
Josh <zylo.codes@gmail.com>
2022-10-06 14:41:01 -0500

    Array.iter

--- helloworld/Program.fs
+++ helloworld/Program.fs
@@ -1,8 +1,10 @@
 ﻿open System
 
+let sayHello person =
+  printfn "Hello %s from my F# program!" person
+
 [<EntryPoint>]
 let main argv =
-  for person in argv do 
-    printfn "Hello %s from my F# program!" person
+  Array.iter sayHello argv
   printfn "Nice to meet you."
   0
