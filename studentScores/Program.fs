b3a3778c7c62aeaa7aea920a3ba35bb251a5ac3f
Josh <zylo.codes@gmail.com>
2022-10-09 09:42:33 -0500

    mean score

--- studentScores/Program.fs
+++ studentScores/Program.fs
@@ -1,11 +1,23 @@
 ﻿open System
 open System.IO
 
+let printMeanScore (row: string) =
+  let elements = row.Split('\t')
+  let id = elements.[0]
+  let name = elements.[1]
+  let meanScore = 
+    elements
+    |> Array.skip 2
+    |> Array.map float
+    |> Array.average
+  printfn "%s\t%s\t%0.1f" name id meanScore
+
 let summarize filePath =
   let rows = File.ReadAllLines filePath
   let studentCount = (rows |> Array.length) - 1
-  for row in rows do
-    printfn "%i %s" studentCount row
+  rows
+  |> Array.skip 1
+  |> Array.iter printMeanScore
 
 [<EntryPoint>]
 let main argv = 
