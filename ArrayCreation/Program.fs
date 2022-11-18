a482f0e167e9403bdefd06afc08740fa3763a7ec
Josh <zylo.codes@gmail.com>
2022-11-18 14:28:45 -0600

    ArrayCreation

---
+++ ArrayCreation/Program.fs
@@ -0,0 +1,49 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+
+  let isEven x =
+    x % 2 = 0
+
+  let todayIsThursday() =
+    DateTime.Now.DayOfWeek = DayOfWeek.Thursday
+
+  // let numbers = [| 1; 2; 4; 8; 16 |]
+
+  let numbers =
+    [|
+      if todayIsThursday() then 42
+      for i in 1..9 do
+        let x = i * i
+        if x |> isEven then
+          x
+      999
+    |]
+
+  printfn "%A" numbers
+
+  let total =
+    [| for i in 1..1000 do yield i * i|]
+    |> Array.sum
+
+  printfn "%i" total
+
+  let numbers2 = Array.init 5 (fun i -> pown 2 i)
+  printfn "%A" numbers2
+
+  let initiallyZeros = Array.zeroCreate<int> 10
+  initiallyZeros.[0] <- 42
+  printfn "%A" initiallyZeros
+
+
+  // Example of reading Stream of data (eg Audio) 
+  // let readByts length (stream: Stream) =
+  //   let buffer = Array.zeroCreate<byte> length
+  //   let count = stream.Read(buffer, 0, length)
+  //   if count = length then
+  //     buffer
+  //   else
+  //     raise <| EndOfStreamException()
+
+  0
--- studentScores/Summary.fs
+++ studentScores/Summary.fs
@@ -4,11 +4,23 @@ module Summary =
 
   open System.IO
 
+  let printGroupSummary (surname: string) (students: Student[]) =
+    printfn "%s" (surname.ToUpperInvariant())
+    students
+    |> Array.sortBy (fun student -> 
+      student.GivenName, student.Id)
+    |> Array.iter (fun student -> 
+      printfn "\t%20s\t%s\t%0.1f\t%0.1f\t%0.1f" 
+        student.GivenName student.Id student.MeanScore  student.MinScore student.MaxScore)
+
   let summarize filePath =
     let rows = File.ReadAllLines filePath
     let studentCount = (rows |> Array.length) - 1
+    printfn "Student count %i" studentCount
     rows
     |> Array.skip 1
     |> Array.map Student.fromString
-    |> Array.sortBy (fun student -> student.GivenName)
-    |> Array.iter Student.printMeanScore
+    |> Array.groupBy (fun s -> s.Surname)
+    |> Array.sortBy fst
+    |> Array.iter (fun (surname, students) -> 
+      printGroupSummary surname students)
