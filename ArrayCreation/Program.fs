12cc554a1dd9b06bc889dcb9dacd4042358c54b7
Josh <zylo.codes@gmail.com>
2022-11-28 08:50:03 -0600

    Seq

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -3,38 +3,40 @@
 [<EntryPoint>]
 let main argv =
 
-  let isEven x =
-    x % 2 = 0
+  // Arrays
 
-  let todayIsThursday() =
-    DateTime.Now.DayOfWeek = DayOfWeek.Thursday
+  // let isEven x =
+  //   x % 2 = 0
 
-  // let numbers = [| 1; 2; 4; 8; 16 |]
+  // let todayIsThursday() =
+  //   DateTime.Now.DayOfWeek = DayOfWeek.Thursday
 
-  let numbers =
-    [|
-      if todayIsThursday() then 42
-      for i in 1..9 do
-        let x = i * i
-        if x |> isEven then
-          x
-      999
-    |]
+  // // let numbers = [| 1; 2; 4; 8; 16 |]
 
-  printfn "%A" numbers
+  // let numbers =
+  //   [|
+  //     if todayIsThursday() then 42
+  //     for i in 1..9 do
+  //       let x = i * i
+  //       if x |> isEven then
+  //         x
+  //     999
+  //   |]
 
-  let total =
-    [| for i in 1..1000 do yield i * i|]
-    |> Array.sum
+  // printfn "%A" numbers
 
-  printfn "%i" total
+  // let total =
+  //   [| for i in 1..1000 do yield i * i|]
+  //   |> Array.sum
 
-  let numbers2 = Array.init 5 (fun i -> pown 2 i)
-  printfn "%A" numbers2
+  // printfn "%i" total
 
-  let initiallyZeros = Array.zeroCreate<int> 10
-  initiallyZeros.[0] <- 42
-  printfn "%A" initiallyZeros
+  // let numbers2 = Array.init 5 (fun i -> pown 2 i)
+  // printfn "%A" numbers2
+
+  // let initiallyZeros = Array.zeroCreate<int> 10
+  // initiallyZeros.[0] <- 42
+  // printfn "%A" initiallyZeros
 
 
   // Example of reading Stream of data (eg Audio) 
@@ -46,4 +48,14 @@ let main argv =
   //   else
   //     raise <| EndOfStreamException()
 
+
+  // Sequences
+
+  let total = 
+    seq { for i in 1..1000 -> i * i}
+    |> Seq.sum
+  
+  printfn "The total is: %i" total
+  
+  
   0
